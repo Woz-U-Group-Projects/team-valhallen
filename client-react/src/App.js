bd19236ea5d3e47c0a07e67a7af1a269b37a3ee5
Johnny San Nicolas <johnny.sannicolas@my.scitexas.edu>
2020-04-16 21:41:24 -0700

    model fix

--- client-react/src/App.js
+++ client-react/src/App.js
@@ -12,7 +12,7 @@ import UserSignUp from"./components/UserSignUp";
 function App() {
   return (
     <div className="App">
-      
+      <UserSignUp />
       <Login />
       <UserPage />
     </div>
