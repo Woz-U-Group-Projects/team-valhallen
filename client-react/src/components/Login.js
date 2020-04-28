dc95a7afc7e5321a0b77b54c9ca4c3c13366ff1a
Johnny San Nicolas <johnny.sannicolas@my.scitexas.edu>
2020-04-27 20:38:49 -0700

    auth check

--- client-react/src/components/Login.js
+++ client-react/src/components/Login.js
@@ -25,7 +25,7 @@ class Login extends React.Component {
         this.state = {
             users: [],
             user: {},
-            // loggedIn: false,
+            loggedIn: false,
             value: 'tenant'
         };
         this.email = React.createRef();
