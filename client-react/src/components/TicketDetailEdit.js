16355b021381b974f4cdbf9053a4ed82987066eb
Ramanus <ramanusonly@gmail.com>
2020-04-30 21:46:08 -0500

    edited ticket list buttons

--- client-react/src/components/TicketDetailEdit.js
+++ client-react/src/components/TicketDetailEdit.js
@@ -42,37 +42,6 @@ const TicketDetailEdit = (props) => {
       </div></Card.Footer>
         </Card>
 
-      {/* <h3>Ticket Detail</h3>
-      <div>
-        <h4>Ticket Id: { props.ticketDetail.ticketId }</h4>
-        <h4>Unit Id:{ props.ticketDetail.unitId }</h4>
-        <h5>Category:{ props.ticketDetail.category }</h5>
-        <h4>Priority Level:{ props.ticketDetail.priority }</h4>
-        <h4>Current Ticket Status:{ props.ticketDetail.status }</h4>
-        <h5>Tenant Problem Notes:{ props.ticketDetail.note }</h5>
-      </div>
-      <form>
-        <label htmlFor="status">Status</label>
-        <select type="text" name="status" value={status} onChange={event => dispatch({ type: UPDATE_TICKET_STATUS, payload: event.target.value })}>Change Status
-                <option value="inProgress">
-                  In progress
-                </option>
-                <option value="onHold">
-                  On hold
-                </option>
-                <option value="complete">
-                  Complete
-                </option>
-                <option value="inComplete">
-                  In complete
-                </option>
-         </select>
-        <label htmlFor="mainNote">mainNote</label>
-        <input type="text" name="mainNote" value={mainNote} onChange={event => dispatch({ type: UPDATE_TICKET_NOTE, payload: event.target.value })} />
-      </form>
-      <div>
-        <button type="button" className="btn btn-primary" data-id={ props.ticketDetail.ticketId } data-status={ status } data-note={ mainNote } onClick={ props.updateCall }>Update Ticket</button>
-      </div> */}
     </div>
   );
 }
