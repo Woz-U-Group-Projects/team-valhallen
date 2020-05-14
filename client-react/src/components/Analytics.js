d4748374a423153d602011aeb625763c9d00677b
Ramanus <ramanusonly@gmail.com>
2020-05-14 13:30:52 -0500

    analytics components for the stretch goal

--- client-react/src/components/Analytics.js
+++ client-react/src/components/Analytics.js
@@ -2,17 +2,18 @@ import React from "react";
 // import axios from "axios";
 import '../task.min.css'
 import Card from "react-bootstrap/Card"
+import { CardGroup, Table } from "react-bootstrap";
 
 class Analytics extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { 
-            pastDueTkts: [], 
-            qtyPendTkts: 'numberOfTickets', 
-            backlogTech: 'techName', 
+        this.state = {
+            pastDueTkts: [],
+            qtyPendTkts: 'numberOfTickets',
+            backlogTech: 'techName',
             backlogQty: 'numberOfTickets',
             busyUnits: 'unitName',
-            problemArea: 'category name'  
+            problemArea: 'category name'
         };
     }
 
@@ -23,18 +24,149 @@ class Analytics extends React.Component {
     render() {
         return (
             <div>
-                <Card className="text-center" style={{ width: '18 rem' }}>
-                    <Card.Body>
-                <h6>New Tickets # {this.props.newTicketsNumber} 
-                || Pending Tickets # {this.props.pendingTicketsNumber} 
-                || In Progress Ticktes# {this.props.inProgressTicketsNumber}
-                || On Hold Tickets# {this.props.onHoldTicketsNumber} 
-                || Completed Tickets # {this.props.completedTicketsNumber}
-                || Archived Tickets # {this.props.archivedTicketsNumber}
-                </h6>
-                </Card.Body>
-                </Card>
-            </div>
+                <CardGroup>
+                    <Card className="text-center" style={{ width: '18 rem' }}>
+                        <Card.Header as="h5">Tech 1</Card.Header>
+                        <Card.Body>
+                            <Table striped bordered hover size="sm">
+                                <thead>
+                                    <tr>
+                                        <td>Work Load </td>
+                                        <td># of Tickets</td>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Pending</td>
+                                        <td>{this.props.pendingTicketsNumber}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>In-Progress</td>
+                                        <td>{this.props.inProgressTicketsNumber}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>On-Hold</td>
+                                        <td>{this.props.onHoldTicketsNumber}</td>
+                                    </tr>
+
+                                </tbody>
+                            </Table>
+                        </Card.Body>
+                    </Card>
+                    <Card className="text-center" style={{ width: '18 rem' }}>
+                        <Card.Header as="h5">Tech 2</Card.Header>
+                        <Card.Body>
+                            <Table striped bordered hover size="sm">
+                                <thead>
+                                    <tr>
+                                        <td>Work Load </td>
+                                        <td># of Tickets</td>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Pending</td>
+                                        <td>{this.props.pendingTicketsNumber}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>In-Progress</td>
+                                        <td>{this.props.inProgressTicketsNumber}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>On-Hold</td>
+                                        <td>{this.props.onHoldTicketsNumber}</td>
+                                    </tr>
+
+                                </tbody>
+                            </Table>
+                        </Card.Body>
+                    </Card>
+                    <Card className="text-center" style={{ width: '18 rem' }}>
+                        <Card.Header as="h5">Tech 3</Card.Header>
+                        <Card.Body>
+                            <Table striped bordered hover size="sm">
+                                <thead>
+                                    <tr>
+                                        <td>Work Load </td>
+                                        <td># of Tickets</td>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Pending</td>
+                                        <td>{this.props.pendingTicketsNumber}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>In-Progress</td>
+                                        <td>{this.props.inProgressTicketsNumber}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>On-Hold</td>
+                                        <td>{this.props.onHoldTicketsNumber}</td>
+                                    </tr>
+
+                                </tbody>
+                            </Table>
+                        </Card.Body>
+                    </Card>
+                    <Card className="text-center" style={{ width: '18 rem' }}>
+                        <Card.Header as="h5">Tech 4</Card.Header>
+                        <Card.Body>
+                            <Table striped bordered hover size="sm">
+                                <thead>
+                                    <tr>
+                                        <td>Work Load </td>
+                                        <td># of Tickets</td>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Pending</td>
+                                        <td>{this.props.pendingTicketsNumber}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>In-Progress</td>
+                                        <td>{this.props.inProgressTicketsNumber}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>On-Hold</td>
+                                        <td>{this.props.onHoldTicketsNumber}</td>
+                                    </tr>
+
+                                </tbody>
+                            </Table>
+                        </Card.Body>
+                    </Card>
+                    <Card className="text-center" style={{ width: '18 rem' }}>
+                        <Card.Header as="h5">Tech 5</Card.Header>
+                        <Card.Body>
+                            <Table striped bordered hover size="sm">
+                                <thead>
+                                    <tr>
+                                        <td>Work Load </td>
+                                        <td># of Tickets</td>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>Pending</td>
+                                        <td>{this.props.pendingTicketsNumber}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>In-Progress</td>
+                                        <td>{this.props.inProgressTicketsNumber}</td>
+                                    </tr>
+                                    <tr>
+                                        <td>On-Hold</td>
+                                        <td>{this.props.onHoldTicketsNumber}</td>
+                                    </tr>
+
+                                </tbody>
+                            </Table>
+                        </Card.Body>
+                    </Card>
+                </CardGroup>
+            </div >
         );
     }
 }
